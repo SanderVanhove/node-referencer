560a94bc4253224b3ba07be4f4842177c4f7146e
Sander Vanhove <sandervhove@gmail.com>
2022-03-10 13:25:05 +0100

    Clean up vscode settings

--- Test.gd
+++ Test.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 ### Automatic References Start ###
-onready var _ray_cast_2d: RayCast2D = $AnimatedSprite/RayCast2D
+onready var _collision_shape_2d: CollisionShape2D = $Node2D/KinematicBody2D/CollisionShape2D
+onready var _ray_cast_2d: RayCast2D = $Node2D/AnimatedSprite/RayCast2D
 ### Automatic References Stop ###
 
 const A_CONSTANT: int = 4
 
 func _ready() -> void:
-	_ray_cast_2d.position = Vector2.ZERO
+	pass
